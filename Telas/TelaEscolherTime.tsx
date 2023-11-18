cc05bfcd074251a3bf84382cb3298a5a0a947e52
lucas <LucasMaioli12@gmail.com>
2023-11-17 22:42:02 -0300

    Logica do formartime pronta

--- Telas/TelaEscolherTime.tsx
+++ Telas/TelaEscolherTime.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { View, StyleSheet, TouchableOpacity, TextInput, FlatList, Text, Dimensions } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, TextInput, FlatList, Text, Dimensions, Alert } from 'react-native';
 import COLORS from '../constants/colors';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import Header from '../constants/Header';
@@ -18,6 +18,11 @@ const TelaEscolherTime = () => {
   const [selectedPosition, setSelectedPosition] = useState(null);
   const [checkedAll, setCheckedAll] = useState(false);
   const [checkedItems, setCheckedItems] = useState({});
+  const [selectedButton, setSelectedButton] = useState('nomes');
+  const [isSecondModalVisible, setSecondModalVisible] = useState(false);
+  const [number, setNumber] = useState(0);
+  const [teams, setTeams] = useState([]);
+
 
 
   useEffect(() => {
@@ -105,6 +110,44 @@ const TelaEscolherTime = () => {
     setCheckedAll(false);
   };
 
+  const handleButtonPress = (button) => {
+    setSelectedButton(button);
+  };
+
+  const toggleSecondModal = () => {
+    setSecondModalVisible(!isSecondModalVisible);
+  };
+
+
+
+  const formTeams = () => {
+    if (number <= 0) {
+      Alert.alert('Erro', 'Por favor, insira um número válido para formar os times.');
+      return;
+    }
+
+    const selectedPlayers = data.filter((item) => checkedItems[item.key]);
+
+    if (selectedPlayers.length < number) {
+      Alert.alert('Erro', 'Não há jogadores suficientes para formar times.');
+      return;
+    }
+
+    const shuffledPlayers = selectedPlayers.sort(() => Math.random() - 0.5);
+
+    const newTeams = [];
+    const playersPerTeam = Math.ceil(shuffledPlayers.length / number);
+
+    for (let i = 0; i < number; i++) {
+      newTeams.push(shuffledPlayers.splice(0, playersPerTeam));
+    }
+
+    setTeams(newTeams);
+    setNumber(0)
+    toggleSecondModal();
+  };
+
+
   return (
     <View style={{ flex: 1 }}>
       <StatusBar translucent backgroundColor="rgba(0,0,0,0)" />
@@ -114,7 +157,7 @@ const TelaEscolherTime = () => {
         </TouchableOpacity>
         <View style={{ flexDirection: 'row' }}>
           <View style={{ height: '100%', width: '80%', justifyContent: 'center' }}>
-            <Text style={{ marginTop: 13, fontSize: 20, fontWeight: 'bold',color:COLORS.primary }}>Formar Time</Text>
+            <Text style={{ marginTop: 13, fontSize: 20, fontWeight: 'bold', color: COLORS.primary }}>Formar Time</Text>
           </View>
           <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 13, }}>
             <TouchableOpacity style={styles.btnHeader} onPress={toggleModal}>
@@ -123,7 +166,7 @@ const TelaEscolherTime = () => {
           </View>
         </View>
       </View>
-      <View style={{ borderBottomWidth: 1, borderBottomColor: COLORS.LIGHT_GRAY, paddingHorizontal: 10, paddingVertical: 6, marginBottom: 10, flexDirection: 'row', justifyContent: 'space-between', paddingEnd: 30 }}>
+      <View style={{ borderBottomWidth: 1, borderBottomColor: COLORS.LIGHT_GRAY, paddingHorizontal: 10, paddingVertical: 6, flexDirection: 'row', justifyContent: 'space-between', paddingEnd: 30 }}>
         <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
           <CheckBox
             checked={checkedAll}
@@ -134,14 +177,17 @@ const TelaEscolherTime = () => {
           />
           <Text style={{ color: COLORS.secondary, fontSize: 14 }}>Selecionar tudo</Text>
         </View>
-
-
-
         <TouchableOpacity style={{ justifyContent: 'center', alignItems: 'center', marginLeft: 30 }} onPress={data.length > 0 ? clearSelectedData : null} disabled={Object.keys(checkedItems).length === 0}>
           <Ionicons name="trash-bin-outline" size={20} color={COLORS.primary} />
         </TouchableOpacity>
-
-
+      </View>
+      <View style={{ borderBottomWidth: 1, justifyContent: 'space-between', flexDirection: 'row', borderBottomColor: COLORS.LIGHT_GRAY }}>
+        <TouchableOpacity style={{ justifyContent: 'center', alignItems: 'center', width: '50%', paddingStart: 40, borderEndWidth: 0.5, borderEndColor: COLORS.LIGHT_GRAY, backgroundColor: selectedButton === 'nomes' ? `${COLORS.secondary}50` : 'transparent', }} onPress={() => handleButtonPress('nomes')}>
+          <Text>Nomes</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{ justifyContent: 'center', alignItems: 'center', width: '50%', paddingEnd: 40, borderStartWidth: 0.5, borderStartColor: COLORS.LIGHT_GRAY, height: 40, backgroundColor: selectedButton === 'times' ? `${COLORS.secondary}50` : 'transparent', }} onPress={() => handleButtonPress('times')}>
+          <Text style={{}}>Times</Text>
+        </TouchableOpacity>
       </View>
       <View style={{ alignItems: 'center', justifyContent: 'center' }}>
         <Modal isVisible={isModalVisible} onBackdropPress={toggleModal}>
@@ -217,29 +263,78 @@ const TelaEscolherTime = () => {
           </View>
         </Modal>
       </View>
-      <FlatList
-        data={data}
-        renderItem={({ item }) => (
-          <View style={[styles.btn, { flexDirection: 'row', alignItems: 'center' }]}>
-            <CheckBox
-              checked={checkedItems[item.key] || false}
-              onPress={() => handleCheckboxChange(item.key)}
-              checkedColor={COLORS.secondary}
-              uncheckedColor={COLORS.secondary}
-              size={30}
-              containerStyle={{ backgroundColor: 'transparent', borderWidth: 0, marginRight: 10, padding: 0 }}
-            />
-            <View style={styles.bola}>
-              <Text style={{ fontWeight: '900', color: 'white', fontSize: 20 }}>{item.position}</Text>
-            </View>
-            <Text style={{ fontSize: 15, fontWeight: 'bold', flex: 1 }}>{item.name}</Text>
-          </View>
+      {selectedButton === 'nomes' ? (
+        <View style={{ marginTop: 15 }}>
+          <FlatList
+            data={data}
+            renderItem={({ item }) => (
+              <View style={[styles.btn, { flexDirection: 'row', alignItems: 'center' }]}>
+                <CheckBox
+                  checked={checkedItems[item.key] || false}
+                  onPress={() => handleCheckboxChange(item.key)}
+                  checkedColor={COLORS.secondary}
+                  uncheckedColor={COLORS.secondary}
+                  size={30}
+                  containerStyle={{ backgroundColor: 'transparent', borderWidth: 0, marginRight: 10, padding: 0 }}
+                />
+                <View style={styles.bola}>
+                  <Text style={{ fontWeight: '900', color: 'white', fontSize: 20 }}>{item.position}</Text>
+                </View>
+                <Text style={{ fontSize: 15, fontWeight: 'bold', flex: 1 }}>{item.name}</Text>
+              </View>
 
-        )}
-      />
-      <View style={styles.bottomView}>
-        <Text style={{ color: COLORS.white, fontSize: 20, fontWeight: 'bold' }}>Formar o Time</Text>
-      </View>
+            )}
+
+          />
+        </View>) : (
+
+        <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+          <Modal isVisible={isSecondModalVisible} onBackdropPress={toggleSecondModal}>
+            <View style={{ backgroundColor: 'white', paddingHorizontal: 10, paddingVertical: 10, borderRadius: 10 }}>
+              <View style={{ marginBottom: 8 }}>
+                <Ionicons name="close" size={30} color={COLORS.primary} onPress={toggleSecondModal} />
+              </View>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between', }}>
+                <TextInput
+                  style={{ height: 40, width: 60, borderColor: COLORS.secondary, borderWidth: 2, paddingLeft: 10, borderRadius: 10 }}
+                  placeholder="1-10"
+                  keyboardType="numeric"
+                  value={String(number)}
+                  onChangeText={(text) => {
+                    const parsedNumber = parseInt(text, 10);
+                    setNumber(isNaN(parsedNumber) ? 0 : parsedNumber);
+                  }}
+                />
+                <TouchableOpacity style={{ height: 50, width: 65, backgroundColor: COLORS.secondary, borderRadius: 10, justifyContent: 'center', alignItems: 'center' }} onPress={formTeams}>
+                  <Text style={{ color: COLORS.white, fontWeight: 'bold' }}>Formar</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Modal>
+          <View style={{ flex: 1, paddingHorizontal: 10, paddingVertical: 20 }}>
+            <FlatList
+              data={teams}
+              renderItem={({ item, index }) => (
+                <View style={{ marginBottom: 20, borderRadius: 10, width: 370 }}>
+                  <View style={{ alignItems: 'center', justifyContent: 'center', padding: 20, backgroundColor: 'red', borderRadius: 10 }}>
+                    <Text style={{ fontWeight: '900', color: COLORS.white }}>{`Time ${index + 1}`}</Text>
+                  </View>
+                  <View style={{ padding: 20 }}>
+                    {item.map((player) => (
+                      <Text key={player.key}>{player.name}</Text>
+                    ))}
+                  </View>
+                </View>
+              )}
+              keyExtractor={(item, index) => index.toString()}
+            />
+          </View>
+          <TouchableOpacity style={styles.bottomView} onPress={toggleSecondModal}>
+            <Text style={{ color: COLORS.white, fontSize: 20, fontWeight: 'bold' }}>Formar o Time</Text>
+          </TouchableOpacity>
+        </View>
+
+      )}
     </View>
   );
 };
